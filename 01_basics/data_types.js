74c71e01b700983e911827bb5548de4136397beb
Nasir Iqbal <nasiriqbal1198@gmail.com>
2025-01-09 19:13:46 +0500

    JavaScript primitive data types

--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -1,10 +1,18 @@
+// Primitive Data Types in JavaScript
+let name = "nasir";         // String: Represents text
+let number = 10;            // Number: Represents numbers
+let isLoggedIn = false;     // Boolean: true or false
+let state = null;           // Null
+let undefinedVar;           // Undefined
+let uniqueId = Symbol("id");
+let uniqueId2=Symbol("id"); // Symbol: Unique value
+let bigNumber = 123n;       // BigInt: 
 
-
-let name = "nasir";  // String
-let number = 10;  // Number
-let isLoggedIn=false; //boolean
-let state=null;
-let undefined;
-
-
-console.log(typeof undefined)
+console.log(typeof name);         // Outputs: string
+console.log(typeof number);       // Outputs: number
+console.log(typeof isLoggedIn);   // Outputs: boolean
+console.log(typeof state);        // Outputs: object 
+console.log(typeof undefinedVar); // Outputs: undefined
+console.log(typeof uniqueId);     // Outputs: symbol
+console.log(typeof bigNumber);    // Outputs: bigint
+console.log(uniqueId===uniqueId2,) //Outputs:false;
