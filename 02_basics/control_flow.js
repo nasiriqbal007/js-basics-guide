c2f078222e546a97fc934bd8685ac881d165d138
Nasir Iqbal <nasiriqbal1198@gmail.com>
2025-01-11 13:27:28 +0500

    Add control flow examples in JavaScript with comments

---
+++ 02_basics/control_flow.js
@@ -0,0 +1,50 @@
+let age=17;
+// if (age>18) {
+//     console.log("adult")
+// } else {
+//     console.log("teen")
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+
+    const a =2;
+    // console.log(`${i}: ${i*a}`);
+    
+}
+let i=1;
+while (i<=10) {
+    const a=3;
+    // console.log(`${i}: ${i*a}`);
+    i++;
+    
+}
+let j=1;
+do {
+    
+    const a=4;
+    // console.log(`${j}: ${j*a}`);
+    j++;
+   
+} while (j<=10 );
+
+
+let day=2;
+// switch (day) {
+//     case 1:
+//         console.log("Monday")
+//         break;
+// case 2:
+//     console.log("Tuesday")
+//     break;
+//     case 3:
+//         console.log("Wednesday")
+//     default:
+//         "Invalid Value"
+//         break;
+// }
+
+
+
+let score=80;
+score>70 ? console.log("FAIL") :console.log("PASS"); 
